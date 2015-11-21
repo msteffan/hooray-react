9abcc27029c05f9f3e254ca029ccfe0098ce9359
Melissa Steffan <msteffan2012@gmail.com>
2015-11-21 12:46:30 -0500

    adds submit action for form

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,8 @@ var Router  = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
 var createBrowserHistory = require('history/lib/createBrowserHistory')
+var helper = require('./helpers')
+var History = ReactRouter.History;
 
 // App
 var App = React.createClass({
@@ -58,15 +60,24 @@ var Inventory = React.createClass({
 
 // store picker <StorePicker>
 var StorePicker = React.createClass({
-  render : function(){
+  mixins : [History],
+  goToStore : function(event) {
+    event.preventDefault();
+    // get the data from the input
+    var storeId = this.refs.storeId.value;
+    // transition from StorePicker to App
+    this.history.pushState(null, "/store/" + storeId)
+  },
+  render : function() {
     return (
-      <form className="store-selector">
-        <h2>Please enter a store</h2>
-          <input type="text" ref="storeId" required />
-          <input type="submit" />
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" defaultValue={helper.getFunName()} required />
+        <input type="Submit" />
       </form>
     )
   }
+
 });
 
 // 404 component
@@ -77,6 +88,7 @@ var NotFound = React.createClass({
     )
   }
 })
+
 // Routes
 var routes = (
   <Router history={createBrowserHistory()}>
