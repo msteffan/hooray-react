c2d9032e82cbb5bf7671f95ee39e80a83073ceda
Melissa Steffan <msteffan2012@gmail.com>
2015-11-21 11:36:54 -0500

    create initial react components including app, header, order, inventory

---
+++ scripts/main.js
@@ -0,0 +1,69 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+// App
+var App = React.createClass({
+    render : function (){
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="A lovely little market"/>
+                </div>
+                <Order />
+                <Inventory />
+
+            </div>
+        )
+    }
+})
+
+// Header
+var Header = React.createClass({
+    render : function(){
+        return (
+            <header className="top">
+                <h1>Catch
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                Day</h1>
+                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+
+            </header>
+        )
+    }
+})
+
+// Order
+var Order = React.createClass({
+    render : function(){
+        return (
+            <p>Order</p>
+        )
+    }
+})
+
+// Inventory
+var Inventory = React.createClass({
+    render : function(){
+        return (
+            <p>Inventory</p>
+        )
+    }
+})
+
+// store picker <StorePicker>
+var StorePicker = React.createClass({
+    render : function(){
+        return (
+            <form className="store-selector">
+                <h2>Please enter a store</h2>
+                <input type="text" ref="storeId" required />
+                <input type="submit" />
+            </form>
+        )
+    }
+});
+
+ReactDOM.render(<App/>, document.querySelector("#main"))
