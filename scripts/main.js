7d82b5f186bee2e400fc1b034db739af0f495596
Melissa Steffan <msteffan2012@gmail.com>
2015-11-22 11:39:47 -0500

    updated storeId to be prop rather than param

--- scripts/main.js
+++ scripts/main.js
@@ -28,17 +28,17 @@ var App = React.createClass({
     }
   },
   componentDidMount : function(){
-    // base.syncState(this.props.params.storeId + '/fishes', {
-    //   context: this,
-    //   state: 'fishes'
-    // });
-    // var localStorageRef = localStorage.getItem('order-' + this.props.params.storeId)
-    // if (localStorageRef){
-    //   this.setState({order: JSON.parse(localStorageRef) })
-    // }
+    base.syncState(this.props.storeId + '/fishes', {
+      context: this,
+      state: 'fishes'
+    });
+    var localStorageRef = localStorage.getItem('order-' + this.props.storeId)
+    if (localStorageRef){
+      this.setState({order: JSON.parse(localStorageRef) })
+    }
   },
   componentWillUpdate : function(nextProps, nextState){
-    // localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
+    localStorage.setItem('order-' + this.props.storeId, JSON.stringify(nextState.order));
   },
   addFish : function(fish){
     var timestamp = (new Date()).getTime();
@@ -265,4 +265,4 @@ var Inventory = React.createClass({
 //   </Router>
 // )
 
-ReactDOM.render(<App />, document.querySelector("#main"))
+ReactDOM.render(<App storeId="myStore"/>, document.querySelector("#main"))
