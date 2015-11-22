e9ec4b990bae549dca829c89cbd3173aded37034
Melissa Steffan <msteffan2012@gmail.com>
2015-11-22 17:40:02 -0500

    serve static files correctly

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-app.get('/', function(req, res) {
-     res.sendFile("/index.html");
+var express = require("express");
+var app = express();
+var path = require("path");
 
+
+app.use(express.static('build'));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
 
-  //
-  // app.listen(process.env.PORT || 3000, function(){
-  //   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  // });
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
